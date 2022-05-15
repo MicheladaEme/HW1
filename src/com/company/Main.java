6aacee2a14bdaad777db0331ef4f14812ac103b4
manza <artemisholmes32@gmail.com>
2022-05-15 09:42:53 -0500

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i=0; i<101; i++){
+        for (int i=1; i<51; i++){
             System.out.println(i);
         }
     }
